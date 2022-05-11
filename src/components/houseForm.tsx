a0aef15a513550b1aef0c0514bec4d438178f10c
Your Name <mamsheikh123@gmail.com>
2022-05-11 12:34:38 +0100

    house form

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,8 +1,8 @@
-// import { useState, useEffect, ChangeEvent } from "react";
-// import { useForm } from "react-hook-form";
+import { useState, useEffect, ChangeEvent } from "react";
+import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
-// import { useRouter } from "next/router";
-// import Link from "next/link";
+import { useRouter } from "next/router";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 // import { SearchBox } from "./searchBox";
 // import {
@@ -15,4 +15,45 @@
 // } from "src/generated/UpdateHouseMutation";
 // import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
 
-export {};
+interface IFormData {
+  address: string;
+  latitude: number;
+  longitude: number;
+  bedrooms: string;
+  image: FileList;
+}
+
+interface IProps {}
+
+export default function HouseForm({}: IProps) {
+  const [submiting, setSubmiting] = useState(false);
+  const { register, handleSubmit, setValue, errors, watch } = useForm<
+    IFormData
+  >({ defaultValues: {} });
+
+  useEffect(() => {
+    register({ name: "address" }, { required: "Please enter your address" });
+    register({ name: "latitude" }, { required: true, min: -90, max: 90 });
+    register({ name: "longitude" }, { required: true, min: -180, max: 180 });
+  }, [register]);
+
+  const handleCreate = async (data: IFormData) => {};
+
+  const onSubmit = (data: IFormData) => {
+    console.log(data);
+    setSubmiting(true);
+    handleCreate(data);
+  };
+  return (
+    <form className="mx-auto  max-w-xl py-4" onSubmit={handleSubmit(onSubmit)}>
+      <h1 className="text-xl">Add a new House</h1>
+      <div className="mt-4">
+        <label htmlFor="search" className="block">
+          Search for you address
+        </label>
+        {/* SEARCH FIELD */}
+        {errors.address && <p>{errors.address.message}</p>}
+      </div>
+    </form>
+  );
+}
