2460d312f464792c66a86fab6aaa6ca50418a61e
Your Name <mamsheikh123@gmail.com>
2022-05-14 12:50:21 +0100

    House Search

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,7 +5,7 @@ import ReactMapGL, { Marker, Popup, ViewState } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useLocalState } from "src/utils/useLocalState";
 import { HousesQuery_houses } from "src/generated/HousesQuery";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 
 interface IProps {
   setDataBounds: (bounds: string) => void;
@@ -46,6 +46,25 @@ export default function Map({ setDataBounds, houses, highlightedId }: IProps) {
           }
         }}
       >
+        <div className="absolute top-0 w-full z-10 p-4">
+          <SearchBox
+            defaultValue=""
+            onSelecteAddress={(_address, latitude, longitude) => {
+              if (latitude && longitude) {
+                setViewport((prev) => ({
+                  ...prev,
+                  latitude,
+                  longitude,
+                  zoom: 12,
+                }));
+                if (mapRef.current) {
+                  const bounds = mapRef.current.getMap().getBounds();
+                  setDataBounds(JSON.stringify(bounds.toArray()));
+                }
+              }
+            }}
+          />
+        </div>
         {houses.map((house) => (
           <Marker
             key={house.id}
